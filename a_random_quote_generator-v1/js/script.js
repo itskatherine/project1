454754ba998980a05ba3f42250ca84c01b05c425
katherine <hurstkatherine@hotmail.co.uk>
2020-05-11 16:43:10 +0100

    added treehouse files

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -1,3 +1,15 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+ ***/
 quotes = [
   {
     quote:
@@ -31,3 +43,20 @@ quotes = [
     year: 1992,
   },
 ];
+
+/***
+ * `getRandomQuote` function
+ ***/
+
+/***
+ * `printQuote` function
+ ***/
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+ ***/
+
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
