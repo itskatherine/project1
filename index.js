02450e0567ad0e30c3ee0c76d218f9792e84a782
katherine <hurstkatherine@hotmail.co.uk>
2020-05-11 15:59:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+quotes = [
+  { quote: "Here's a quote", source: "The source" },
+  { quote: "Here's a quote2", source: "The source2" },
+  { quote: "Here's a quote3", source: "The source3" },
+  { quote: "Here's a quote4", source: "The source4" },
+  { quote: "Here's a quote5", source: "The source5" },
+];
