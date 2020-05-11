6acc7aa9637c094cc192874c7c38ed09a36ef26a
katherine <hurstkatherine@hotmail.co.uk>
2020-05-11 16:35:58 +0100

    Added further quotes

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 quotes = [
-  { quote: "Here's a quote", source: "The source" },
-  { quote: "Here's a quote2", source: "The source2" },
-  { quote: "Here's a quote3", source: "The source3" },
-  { quote: "Here's a quote4", source: "The source4" },
-  { quote: "Here's a quote5", source: "The source5" },
+  {
+    quote:
+      "If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals.",
+    source: "Sirius Black",
+    citation: "Harry Potter and the Goblet of Fire",
+    year: 2000,
+  },
+  {
+    quote: "Your secret is safe with my indifference",
+    source: "Lord Percival Fredrickstein von Musel Klossowski de Rolo III",
+    citation: "Critical Role, C1 Episode 45",
+    year: 2016,
+  },
+  {
+    quote: "Your secret is safe with my indifference",
+    source: "Lord Percival Fredrickstein von Musel Klossowski de Rolo III",
+    citation: "Critical Role, C1 Episode 45",
+    year: 2016,
+  },
+  {
+    quote: "I told you I was ill.",
+    source: "Spike Milligan",
+    citation: "Spike Milligan's gravestone",
+    year: 2002,
+  },
+  {
+    quote: "One small step for a man, one giant leap for mankind",
+    source: "Neil Armstrong",
+    citation: "Moon landing",
+    year: 1992,
+  },
 ];
